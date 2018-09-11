8fb5aebb2826bb94fdf2f4f0e6a1415fdb120a3d
KryukovaLidiya <Lidakrukova1994@gmail.com>
2018-09-12 00:45:27 +0700

    Changed the code style

--- server.cpp
+++ server.cpp
@@ -1,7 +1,7 @@
 #include"server.h"
 
 /*function for start server configuration */
-void runServer(int port) {
+void run_server(int port) {
     const std::unique_ptr<Server> serv(Server::getInstance(port));
     serv->request();
 }
@@ -11,37 +11,37 @@ void runServer(int port) {
  *  second: start server 
  *  the end: delete service file
  */
-int main(int argc, char **argv) 
-{
-    bool isDaemon = false;
-    if(argc < 2 || !isDigit(argv[1])) { //daemon
+int main(int argc, char **argv) {
+    bool is_daemon = false;
+    if(argc < 2 || !is_digit(argv[1])) { //daemon
 	std::cout << "./server port ...." << std::endl;
 	std::cout << ".... - this is options or without" << std::endl;
     	return 0;
     } 
     else if (argc > 2) {
-    	isDaemon = cheackOptions(argc, argv);    
+    	is_daemon = cheack_options(argc, argv);    
     }
 
     int pid = fork();
 
     if(pid < 0) exit(0);
-    if(pid == 0 && isDaemon) {
+    if(pid == 0 && is_daemon) {
 	umask(0);
 	
 	pid_t p = setsid();
-	writePidIntoFile(p);
+	write_pid_into_file(p);
 	
 	chdir("/");
 	close(STDIN_FILENO);
         close(STDOUT_FILENO);
         close(STDERR_FILENO);
 
-	runServer(atoi(argv[1]));
+	run_server(atoi(argv[1]));
     }
     else if(pid > 0 && !isDaemon) {
-    	runServer(atoi(argv[1]));
+    	run_server(atoi(argv[1]));
     }
+
     remove("chatServer.pid");
     return 0;
 }
